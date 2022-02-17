4bd65b2636eb783afba79447f34f62702386f6f0
James Allen <james_allen2@baylor.edu>
2022-02-16 23:17:21 -0600

    actually finished

--- WorldSeriesWinners.py
+++ WorldSeriesWinners.py
@@ -1,5 +1,6 @@
 infile = open("WorldSeriesWinners.txt", "r")
 infile2 = open("WorldSeriesWinners.txt", "r")
+infile3 = open("WorldSeriesWinners.txt", "r")
 
 team = {}
 year = {}
@@ -36,14 +37,22 @@ if specific_year == 1904 or specific_year == 1994:
 else:
     n = specific_year
     x = year[n]
-    t = team[t]
+    for l in infile3:
+        l = l.strip()
+        if l == x:
+            if l in team:
+                team[t] = team[t] + 1
+            else:
+                team[t] = 1
+
+    l = team[t] - 2
     print(
         "The World Series winner in "
         + str(n)
-        + ","
+        + ", "
         + "was the "
         + x
         + ". They've won the World Series "
-        + str(t)
+        + str(l)
         + " time(s)!",
     )
